fb63fbdd384af689e40b924cfd6d9773809cf444
Uthman Dantata <uthmandantata8@gmail.com>
2025-10-24 09:03:21 +0100

    Fix: send email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -4,7 +4,7 @@ export const sendEmail = async (to, subject, html) => {
     try {
         const resend = new Resend('re_huFuajFr_cuo1w6N2gSWPRkKq8a63dGTA');
 
-        resend.emails.send({
+        await resend.emails.send({
             from: `"Your Company" <${process.env.EMAIL_USER}>`,
             to: to,
             subject: subject,

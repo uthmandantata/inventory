9ab8052887d9dc395d026e246a371459b2269f4d
Uthman Dantata <uthmandantata8@gmail.com>
2025-10-25 16:05:58 +0100

    Fix: console log error in create user

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,30 +1,31 @@
 import axios from "axios";
 
 export const sendEmail = async (to, subject, html) => {
-  try {
-    const response = await axios.post(
-      "https://api.brevo.com/v3/smtp/email",
-      {
-        sender: { name: "Inventory App", email: process.env.BREVO_USER },
-        to: [{ email: to }],
-        subject,
-        htmlContent: html,
-      },
-      {
-        headers: {
-          "api-key": process.env.BREVO_PASS,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    try {
+        const response = await axios.post(
+            "https://api.brevo.com/v3/smtp/email",
+            {
+                sender: { name: "Inventory App", email: process.env.BREVO_USER },
+                to: [{ email: to }],
+                subject,
+                htmlContent: html,
+            },
+            {
+                headers: {
+                    "api-key": process.env.BREVO_PASS,
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                },
+            }
+        );
 
-    console.log(`✅ Email sent successfully to ${to}`);
-    return response.data;
-  } catch (error) {
-    console.error(
-      "❌ Error sending email:",
-      error.response?.data || error.message
-    );
-    throw new Error("Email sending failed");
-  }
+        console.log(`✅ Email sent successfully to ${to}`);
+        return response.data;
+    } catch (error) {
+        console.error(
+            "❌ Error sending email:",
+            error.response?.data || error.message
+        );
+        throw new Error("Email sending failed");
+    }
 };

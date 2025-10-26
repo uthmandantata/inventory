c77bb976d5ed9d0eba9b846a000535b0368e0756
Uthman Dantata <uthmandantata8@gmail.com>
2025-10-26 08:25:56 +0100

    FIX: set password page

--- frontend/src/pages/SetPassword.jsx
+++ frontend/src/pages/SetPassword.jsx
@@ -58,7 +58,9 @@ const SetPassword = () => {
                                 <button
                                     type="submit"
                                     className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
-                                    {loading ? "Loading..." : "Sign in"}
+                                    {/* {loading ? "Loading..." : "Sign in"}
+                                     */}
+                                    Sign in
                                 </button>
                             </div>
                         </form>

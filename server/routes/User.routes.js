d101d60528cd28c51e05962375e6dbd96c960e24
Uthman Dantata <uthmandantata8@gmail.com>
2025-10-26 08:22:19 +0100

    Add: set password page

--- server/routes/User.routes.js
+++ server/routes/User.routes.js
@@ -9,7 +9,7 @@ router.get("/", authMiddleware, adminOnly, getAllUsers);
 router.put("/edit-profile", authMiddleware, editMyProfile);
 router.get("/me", authMiddleware, getCurrentUser);
 router.post("/create-user", authMiddleware, adminOnly, createUser);
-router.post("/set-password/", setUserPassword);
+router.post("/set-password", setUserPassword);
 router.delete("/delete-user/:id", authMiddleware, adminOnly, deleteUser);
 
 export default router;
